d6093042ed2ee8f7bdfa33649496d595e52d8096
BiancaTate <tatebianca1204@gmail.com>
2023-11-14 11:41:06 -0500

    "created basic guide and outline for Search Page"

---
+++ scripts/parks.js
@@ -0,0 +1,24 @@
+"use strict"
+
+window.onload = function(_event){
+// const locationOrParkType = document.getElementById("locationOrParkType")
+
+}
+
+function chooseSearchOption(){
+    // present radio buttons that allow user to search by location or park type 
+    // will contain if/else (or maybe switch) that will call either the location or park type function based on user input
+}
+
+function locationSearch(){
+    // runs if user chooses location search option 
+    // produces dropdown of state/territories 
+    // park will match location based on the park's "State Property"
+}
+
+function parkTypeSearch(){
+    // runs if user chooses park type search option
+    // produces dropdown of park types 
+    // park will match description if park's "LocationName" propery CONTAINS the description
+    // will need to check if nationalPark LocationName contain parkTypeData name
+}
