ebfd9d94e05c48bb2c4955a7167cf685d80840ee
BiancaTate <tatebianca1204@gmail.com>
2023-11-15 11:10:37 -0500

    "added additonal functions"

--- scripts/parks.js
+++ scripts/parks.js
@@ -1,45 +1,68 @@
 "use strict"
+const searchChoices = ["Location", "Park Type"]
 
 window.onload = function (_event) {
 
-    // include on change event submitter here in order to access location or park type funct?
+    const searchTypeSelect = document.getElementById("searchTypeSelect")
+    searchTypeSelect.onchange = handleSearchOption
 
-    chooseSearchOption()
+    const location = document.getElementById("location")
+    location.onchange = renderLocationResults
+
+    const parkType = document.getElementById("parkType")
+    parkType.onchange = renderParkTypeResults
+
+    chooseSearchOption(searchChoices, searchTypeSelect)
+    
+
 }
 
-function chooseSearchOption() {
-    const searchChoices = ["Location", "Park Type"]
+function chooseSearchOption(searchChoices, selectElement) {
+
     let html = " <option> Choose a Search Option </option>"
-    
+
     for (let index = 0; index < searchChoices.length; index += 1) {
         let searchChoice = searchChoices[index]
         html += `
         <option value = "${searchChoice}">${searchChoice}</option>
         `
-        // if (searchChoice === searchChoices[0]) {
-    //     locationSearch()
-    // }
-    // else if (searchChoice === searchChoices[1]) {
-    //     parkTypeSearch()
-    // }
-
     }
-    
 
-    const searchTypeSelect = document.getElementById("searchTypeSelect")
-    searchTypeSelect.innerHTML = html
+    selectElement.innerHTML = html
 
+}
 
+function handleSearchOption(event) {
+    const locationOrPark = event.target.value
 
-    // present radio buttons that allow user to search by location or park type 
-    // will contain if/else (or maybe switch) that will call either the location or park type function based on user input
+    if (locationOrPark === searchChoices[0]){
+        locationSearch()
+    }
+    else if (locationOrPark === searchChoices[1]){
+        parkTypeSearch()
+    }
+
 }
 
 function locationSearch() {
-    console.log("location")
-    // runs if user chooses location search option 
-    // produces dropdown of state/territories 
+    let html= "<option> Choose A Location </option>"
+    
+    for (let index = 0; index < locationsArray.length ; index += 1){
+        const location = locationsArray[index]
+        html += `
+        <option value = "${location}"> ${location} </option>        
+        `
+    } 
+
+    let selectElement = document.getElementById("location")
+    selectElement.innerHTML = html
+
     // park will match location based on the park's "State Property"
+}
+
+function renderLocationResults(event){
+    console.log(event)
+
 }
 
 function parkTypeSearch() {
@@ -48,4 +71,9 @@ function parkTypeSearch() {
     // produces dropdown of park types 
     // park will match description if park's "LocationName" propery CONTAINS the description
     // will need to check if nationalPark LocationName contain parkTypeData name
+}
+
+function renderParkTypeResults(event){
+    console.log(event)
+
 }
